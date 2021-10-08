d59dccd60b13e0e732d2f8391598de3bdf5719f3
idaht <ida.trosdahl@gmail.com>
2021-10-08 14:39:45 +0200

    Trying CI

--- Arbkrav5/Oppg1/main_test.c
+++ Arbkrav5/Oppg1/main_test.c
@@ -8,8 +8,8 @@ int main() {
   char *test_gt = ">";
 
   assert(strcmp(replace("&"), "&amp"));
-  assert(strcmp(replace(test_lt), "&lt") == 1);
-  assert(strcmp(replace(test_gt), "&gt") == 1);
+  assert(strcmp(replace(test_lt), "&lt"));
+  assert(strcmp(replace(test_gt), "&gt"));
 
   assert(strcmp(replace(test_amp), "Hei") == 0);
   assert(strcmp(replace(test_lt), "på") == 0);
